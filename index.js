5b053a30246226c6569979387bd62edec95dd8dc
chlkstn <chalkstoneinteractive@gmail.com>
2024-07-03 12:59:23 +0200

    finihshing excercise

---
+++ index.js
@@ -0,0 +1,64 @@
+console.log("Hello world!");
+
+let age = 43;
+
+console.log("this is my" + age);
+
+var myName = "Anton";
+
+const myAge = 43;
+
+let occupation = "unemployed";
+
+console.log(
+  "this is " + myName + " he is " + age + " his occupation is " + occupation
+);
+
+var isMarried = false;
+
+const hisAge = 21;
+
+let homeTown = "stallarholmen";
+
+console.log(isMarried);
+
+console.log(hisAge);
+
+console.log(homeTown);
+
+const name = "pekka";
+
+console.log(name);
+
+/*name = "Johanna";
+
+console.log(name);
+*/
+
+// example of boolean value//
+let isSingle = true;
+
+const notChange = "you cant change this line";
+
+// const cant be changed and we are not risking of overwriting it by changing its value
+
+let number1 = 2;
+let number2 = 4;
+
+let result = number1 + number2;
+
+let result1 = number1 - number2;
+let result2 = number1 * number2;
+let result3 = number1 / number2;
+
+console.log(result1 + ", " + result2 + " ," + result3);
+
+let a = 14;
+
+console.log(a);
+
+a = 5;
+
+console.log(a);
+
+// a = 9 Vi omdeklarerar variablen i a = 7;
